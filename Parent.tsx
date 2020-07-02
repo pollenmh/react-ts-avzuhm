e13f57a1ce7200724a6b702b3d139495bdcc2317
pollenmh <pollenmh@gmail.com>
2020-07-02 14:31:37 +0530

    3 files modified, 1 added

--- Parent.tsx
+++ Parent.tsx
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import  './Page.module.scss';
-import Popup from "reactjs-popup";
+import Popup from "./Popuptest';
 
 interface IData{
   id:string;
@@ -10,60 +10,8 @@ interface IData{
 interface IDataState{
   data1:{};
 }
- const Card = ({ title }) => (
-  <div className="card">
-    <div className="header">{title} position </div>
-    <div className="content">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem
-      sapiente labore architecto exercitationem optio quod dolor cupiditate
-    </div>
-  </div>
-);
-const dialog= () => (
-  <Popup trigger={<button className="button"> Open Modal </button>} modal>
-    {close => (
-      <div className="modal">
-        <a className="close" onClick={close}>
-          &times;
-        </a>
-        <div className="header"> Modal Title </div>
-        <div className="content">
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-          commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-          explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
-        </div>
-        <div className="actions">
-          <Popup
-            trigger={<button className="button"> Trigger </button>}
-            position="top center"
-            closeOnDocumentClick
-          >
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-              magni omnis delectus nemo, maxime molestiae dolorem numquam
-              mollitia, voluptate ea, accusamus excepturi deleniti ratione
-              sapiente! Laudantium, aperiam doloribus. Odit, aut.
-            </span>
-          </Popup>
-          <button
-            className="button"
-            onClick={() => {
-              console.log("modal closed ");
-              close();
-            }}
-          >
-            close modal
-          </button>
-        </div>
-      </div>
-    )}
-  </Popup>
-);
+
+
 class Header extends Component<{},{}>
 {
    
@@ -89,8 +37,9 @@ this.setState({show:1});
 
 render(){  
   //<div>Data : {JSON.stringify(this.state.data1)}</div>   
- return <dialog/>;
-  // return(<ul  className="header">{this.Iterate()}</ul>);
+ return (<div className="example-warper"> <dialog/></div>);
+ 
+   //return(<ul  className="header">{this.Iterate()}</ul>);
 }
 
 loadpopup(){
